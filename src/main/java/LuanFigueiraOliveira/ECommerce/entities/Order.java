59c29bf9f4b4ab6153c55ebffdbd5f53f41c86d4
LuanFigueira <luan.figueira.oliveira@gmail.com>
2025-04-01 17:41:52 -0300

    Entidades Product e Category; Relacionamento muitos-para-muitos

--- src/main/java/LuanFigueiraOliveira/ECommerce/entities/Order.java
+++ src/main/java/LuanFigueiraOliveira/ECommerce/entities/Order.java
@@ -8,15 +8,20 @@ import java.util.Objects;
 @Entity
 @Table(name="tb_order")
 public class Order {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant moment;
+
 	private OrderStatus status;
+
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+
 	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
 	private Payment payment;
 
--- src/main/java/LuanFigueiraOliveira/ECommerce/entities/Payment.java
+++ src/main/java/LuanFigueiraOliveira/ECommerce/entities/Payment.java
@@ -8,11 +8,14 @@ import java.util.Objects;
 @Entity
 @Table(name="tb_payment")
 public class Payment {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant moment;
+
 	@OneToOne
 	@MapsId
 	private Order order;
