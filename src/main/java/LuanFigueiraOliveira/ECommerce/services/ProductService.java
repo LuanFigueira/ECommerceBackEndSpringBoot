0f495b33403ce831242c48929014d54fdf68140a
LuanFigueira <luan.figueira.oliveira@gmail.com>
2025-04-02 17:02:24 -0300

    Consulta paginada com Pageable

--- src/main/java/LuanFigueiraOliveira/ECommerce/services/ProductService.java
+++ src/main/java/LuanFigueiraOliveira/ECommerce/services/ProductService.java
@@ -4,6 +4,8 @@ import LuanFigueiraOliveira.ECommerce.dto.ProductDTO;
 import LuanFigueiraOliveira.ECommerce.entities.Product;
 import LuanFigueiraOliveira.ECommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,5 +22,12 @@ public class ProductService {
 		Optional<Product> result = repository.findById(id);
 		Product product = result.get();
 		return new ProductDTO(product);
+	}
+
+	@Transactional(readOnly = true)
+	public Page<ProductDTO> findAll(Pageable pageable) {
+		Page<Product> products = repository.findAll(pageable);
+		//return products.stream().map(product -> new ProductDTO(product)).toList();
+		return products.map(product -> new ProductDTO(product));
 	}
 }
