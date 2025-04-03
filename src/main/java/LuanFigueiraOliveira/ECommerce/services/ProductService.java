23c052b25eda1833420663468e3334b2290683e7
LuanFigueira <luan.figueira.oliveira@gmail.com>
2025-04-03 11:00:39 -0300

    Funcionalidades CRUD inseridas

--- src/main/java/LuanFigueiraOliveira/ECommerce/services/ProductService.java
+++ src/main/java/LuanFigueiraOliveira/ECommerce/services/ProductService.java
@@ -24,10 +24,42 @@ public class ProductService {
 		return new ProductDTO(product);
 	}
 
+//	@Transactional(readOnly = true)
+//	public List<ProductDTO> findAll() {
+//		List<Product> products = repository.findAll();
+//		return products.stream().map(product -> new ProductDTO(product)).toList();
+//	}
 	@Transactional(readOnly = true)
 	public Page<ProductDTO> findAll(Pageable pageable) {
 		Page<Product> products = repository.findAll(pageable);
-		//return products.stream().map(product -> new ProductDTO(product)).toList();
 		return products.map(product -> new ProductDTO(product));
+	}
+
+	@Transactional
+	public ProductDTO insert (ProductDTO dto) {
+		Product product = new Product();
+		this.copyDtoToEntity(dto, product);
+		product = repository.save(product);
+		return new ProductDTO(product);
+	}
+
+	@Transactional
+	public ProductDTO update(Long id, ProductDTO dto) {
+		Product product = repository.getReferenceById(id);
+		this.copyDtoToEntity(dto, product);
+		product = repository.save(product);
+		return new ProductDTO(product);
+	}
+
+	@Transactional
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+
+	private void copyDtoToEntity(ProductDTO dto, Product product) {
+		product.setName(dto.getName());
+		product.setDescription(dto.getDescription());
+		product.setPrice(dto.getPrice());
+		product.setImgUrl(dto.getImgUrl());
 	}
 }
