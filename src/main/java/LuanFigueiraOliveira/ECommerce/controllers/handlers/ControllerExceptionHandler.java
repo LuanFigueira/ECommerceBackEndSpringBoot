f2cded3ad7b870156caaccee331bc506b833bcad
LuanFigueira <luan.figueira.oliveira@gmail.com>
2025-04-03 19:53:51 -0300

    @Bean Validation; Validações com mensagens personalizadas

--- src/main/java/LuanFigueiraOliveira/ECommerce/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/LuanFigueiraOliveira/ECommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -1,11 +1,14 @@
 package LuanFigueiraOliveira.ECommerce.controllers.handlers;
 
 import LuanFigueiraOliveira.ECommerce.dto.CustomError;
+import LuanFigueiraOliveira.ECommerce.dto.ValidationError;
 import LuanFigueiraOliveira.ECommerce.services.exceptions.DatabaseException;
 import LuanFigueiraOliveira.ECommerce.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -25,6 +28,16 @@ public class ControllerExceptionHandler {
 	public ResponseEntity<CustomError> database(DatabaseException e, HttpServletRequest request) {
 		HttpStatus status = HttpStatus.BAD_REQUEST;
 		CustomError error = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(error);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<CustomError> methodArgumentNotValid(MethodArgumentNotValidException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+		ValidationError error = new ValidationError(Instant.now(), status.value(), "Dados inválidos", request.getRequestURI());
+		for(FieldError f : e.getBindingResult().getFieldErrors()) {
+			error.addError(f.getField(), f.getDefaultMessage());
+		}
 		return ResponseEntity.status(status).body(error);
 	}
 }
--- src/main/java/LuanFigueiraOliveira/ECommerce/dto/ProductDTO.java
+++ src/main/java/LuanFigueiraOliveira/ECommerce/dto/ProductDTO.java
@@ -1,12 +1,24 @@
 package LuanFigueiraOliveira.ECommerce.dto;
 
 import LuanFigueiraOliveira.ECommerce.entities.Product;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public class ProductDTO {
 	private Long id;
+
+	@Size(min = 3, max = 80, message = "Este campo precisa ter, no mínimo, 3 e, no máximo, 80 caracteres")
+	@NotBlank(message = "Este campo precisa ser preenchido")
 	private String name;
+
+	@Size(min = 10, message = "Este campo precisa ter, no mínimo, 10 caracteres")
+	@NotBlank(message = "Este campo precisa ser preenchido")
 	private String description;
+
+	@Positive(message = "Este campo só aceita valores positivos")
 	private Double price;
+
 	private String imgUrl;
 
 	public ProductDTO() {}
